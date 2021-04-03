710ab2c8be07d4138acd49eae7d533c00bd5560e
BhargaviAnnadevara-MSFT <bhargavi.annadevara@microsoft.com>
2021-04-03 21:25:34 +0530

    Module 3 done. All modules complete!

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
 
@@ -15,7 +16,23 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException();
-            return 'F';
+
+            var sortedStudents = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+            var twentyPercentIndex = (int) Math.Round(0.2 * Students.Count);
+            var fortyPercentIndex = (int) Math.Round(0.4 * Students.Count);
+            var sixtyPercentIndex = (int) Math.Round(0.6 * Students.Count);
+            var eightyPercentIndex = (int) Math.Round(0.8 * Students.Count);
+
+            if (averageGrade > sortedStudents[twentyPercentIndex])
+                return 'A';
+            else if (averageGrade > sortedStudents[fortyPercentIndex])
+                return 'B';
+            else if (averageGrade > sortedStudents[sixtyPercentIndex])
+                return 'C';
+            else if (averageGrade > sortedStudents[eightyPercentIndex])
+                return 'D';
+            else
+                return 'F';
         }
 
         public override void CalculateStatistics()
